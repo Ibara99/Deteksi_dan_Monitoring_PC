94d11b347ba4c9e65c5bf970488d8c91246336ae
ibara99 <arsip.ibara@gmail.com>
2021-08-28 03:44:00 +0700

    (by aan) desain dan fungsi minimal untuk btn

--- main.py
+++ main.py
@@ -8,19 +8,15 @@ class Window(QMainWindow, Ui_MainWindow):
         self.connectSignalsSlots()
 
     def connectSignalsSlots(self):
-    ##        self.action_Exit.triggered.connect(self.close)
-    ##        self.action_Find_Replace.triggered.connect(self.findAndReplace)
         self.actionAbout.triggered.connect(self.about) #triger untuk menu
-        self.pushButton.clicked.connect(self.about) #click untuk buton
+        #self.pushButton.clicked.connect(self.about) #click untuk buton
+        
     def about(self):
-        print("diklik")
         QMessageBox.about(
             self,
-            "About Sample Editor",
-            "<p>A sample text editor app built with:</p>"
-            "<p>- PyQt</p>"
-            "<p>- Qt Designer</p>"
-            "<p>- Python</p>",
+            "About",
+            "<p>Aplikasi untuk mengecek hardware danspesifikasi laptop</p>"
+            "<p>- Juga bakal ada alert, mungkin.</p>"
         )
 
 #dialog setting
